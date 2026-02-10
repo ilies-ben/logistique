11704b48cce50bbf6910d1cbbd84772a42ed7eec
ilies <ai.bensalem@esi-sba.dz>
2026-02-10 22:43:15 +0100

    Fix main.py clean

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ import os
 from dotenv import load_dotenv
 from app import create_app, db
 
-# Charger variables d'environnement
 load_dotenv()
 
-# Créer l'application
 app = create_app()
 
-# Export pour Gunicorn
-application = app  # ← RENDERC LE VEUT!
+# Gunicorn
+application = app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
